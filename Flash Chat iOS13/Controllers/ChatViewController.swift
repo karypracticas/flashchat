7db8de59941e3925a763dc82020bbf403f9b0966
Kary Rguez <practicasgithub@gmail.com>
2020-04-17 14:11:14 -0500

    Customizar TableView usando archivo .xib

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -27,6 +27,7 @@ class ChatViewController: UIViewController {
         navigationItem.hidesBackButton =  true
         title = K.appName
         
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
 
     }
     
@@ -53,7 +54,8 @@ extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
 }
